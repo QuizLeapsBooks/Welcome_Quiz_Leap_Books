84d81b6be3e1ed196174c2d21da2501704cf69a3
QuizLeapsBooks <167664682+QuizLeapsBooks@users.noreply.github.com>
2024-07-25 15:51:25 +0530

    Update dynamictext.js

--- dynamictext.js
+++ dynamictext.js
@@ -13,3 +13,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
     type();
 });
+document.getElementById('share-button').onclick = function() {
+    var socialButtons = document.getElementById('social-buttons');
+    if (socialButtons.classList.contains('hidden')) {
+        socialButtons.classList.remove('hidden');
+        socialButtons.classList.add('visible');
+    } else {
+        socialButtons.classList.remove('visible');
+        socialButtons.classList.add('hidden');
+    }
+};
+
+document.getElementById('facebook-share').onclick = function() {
+    window.open(
+        'https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent('https://quizleapsbooks.github.io/Welcome_Quiz_Leap_Books/'), 
+        'facebook-share-dialog', 
+        'width=800,height=600'
+    ); 
+    return false;
+};
+
+document.getElementById('twitter-share').onclick = function() {
+    window.open(
+        'https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + 
+        '&url=' + encodeURIComponent('https://quizleapsbooks.github.io/Welcome_Quiz_Leap_Books/'), 
+        'twitter-share-dialog', 
+        'width=800,height=600'
+    ); 
+    return false;
+};
+
+document.getElementById('linkedin-share').onclick = function() {
+    window.open(
+        'https://www.linkedin.com/sharing/share-offsite/?url=' + encodeURIComponent('https://quizleapsbooks.github.io/Welcome_Quiz_Leap_Books/'), 
+        'linkedin-share-dialog', 
+        'width=800,height=600'
+    ); 
+    return false;
+};
